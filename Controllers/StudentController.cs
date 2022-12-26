5d1cbd80258573c271a13b65a5cd5af991276061
Arie <Rozindzakwan@gmail.com>
2022-12-26 21:58:15 +0700

    Add Mode Static

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using IntegerWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntegerWebApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            var student = new Student()
+            {
+                Id = 1,
+                Name = "Dede",
+                Address = "Ciamis",
+                Phone = "082233824398"
+            };
+            return View(student);
+        }
+    }
+}
